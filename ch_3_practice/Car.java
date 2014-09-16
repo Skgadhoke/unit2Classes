0b69e2403037f5d9497204311ed5a682b9cbe003
Skgadhoke <sim.gadhoke@gmail.com>
2014-09-16 10:49:36 -0500

    Unit 2

---
+++ ch_3_practice/Car.java
@@ -0,0 +1,75 @@
+
+
+/**
+ * A car class tracks fuel capacigy and consumption based on efficiency
+ * 
+ * @author Simar
+ * @version 12 September 2014
+ */
+public class Car
+{
+    /**fuel efficiency of the car measured in miles per gallon (mpg)*/
+    private int fuelEfficiency;
+    
+    /** amount of fuel in gas tank measured in gallons*/
+    private int fuelInTank;
+
+    /**
+     * Constructor that specifies the fuel efficiency of this car
+     */
+    public Car(int efficiency)
+    {
+        // initialise instance variables
+        this.fuelEfficiency = efficiency;
+        this.fuelInTank = 0;
+    }
+
+    /**
+     * The drive method resduces the fuel in the tank based on the specified miles and the
+     * efficiency of this car.
+     * @pre        preconditions for the method
+     *            this method is never invoked with a value with a value for miles that 
+     *            consumes more than the available gas in the tank.
+     *            (what the method guarantees upon completion)
+     * @param   miles number of miles driv
+     * @return    description of the return value/**
+ * An example of a method - replace this comment with your own
+ *  that describes the operation of the method
+ *
+ * @pre     preconditions for the method
+ *          (what the method assumes about the method's parameters and class's state)
+ * @post    postconditions for the method
+ *          (what the method guarantees upon completion)
+ * @param   y   description of parameter y
+ * @return  description of the return value
+ */
+
+    public void drive(int miles)
+    {
+        // put your code here
+        
+   }
+       /**
+     * adds the specified am\ount of gas in gallons to this car's tank
+     * 
+     * @pre     the specified amount of gas doesn't exceed the capackty of this car's tank
+     * 
+     * @param   gallons number of gallons of gas to add to this car's tank
+ */
+public void a(int y)
+    {
+        // put your code here
+        
+    }
+           /**
+     * Returns ht enumber of gallons of gas remaining in this car's tank
+     *
+     * @return   number of gallons remaining
+ */
+public double getGasInTank()
+    {
+        // put your code here
+        return 0;
+    }
+
+}
