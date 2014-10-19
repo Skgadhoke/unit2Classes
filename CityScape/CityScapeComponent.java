b8666af38ecf7cca6abc4c9738df23fbfdcd6c80
Skgadhoke <sim.gadhoke@gmail.com>
2014-10-18 23:04:32 -0500

    code cleanup

--- CityScape/CityScapeComponent.java
+++ CityScape/CityScapeComponent.java
@@ -7,28 +7,23 @@ import javax.swing.JComponent;
 public class CityScapeComponent extends JComponent
 {
     public CityScapeComponent()
-        {
-        
-        }
+    {
     
+    }    
     public void buildingRect()
     {
     
-    }
-    
-    
+    }    
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        //int xCar = 650;
         Skyline skyline = new Skyline();
         skyline.draw(g2);
         
         try {
-        Thread.sleep(1000);
-        //skyline.r
+        	Thread.sleep(1000);
         } catch (Exception e) {
-        e.printStackTrace();
+        	e.printStackTrace();
         }
     
     }
