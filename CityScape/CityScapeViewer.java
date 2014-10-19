435b326e9037fb30133edf8de6d22aa93c67ee57
Skgadhoke <sim.gadhoke@gmail.com>
2014-10-18 21:26:12 -0500

    Update CityScapeViewer.java

--- CityScape/CityScapeViewer.java
+++ CityScape/CityScapeViewer.java
@@ -1,17 +1,19 @@
 import javax.swing.JFrame;
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 public class CityScapeViewer
 {
-    public static int xCar = 650;
-    public static int xCar2 = 0;
+	public static int xCar = 700;
+	public static int xCar2 = -5;
+	public static int birdX = -50;
+	public static int birdY=50;
     /**
-    * main method for the program which creates and configures the frame for the program
-    */
+     * main method for the program which creates and configures the frame for the program
+     */
     public static void main(String[] args)
-        {
+    {
         // create and configure the frame (window) for the program
         JFrame frame = new JFrame();
-        
+             
         frame.setSize(1000 /* x */, 800 /* y */);
         frame.setTitle("Cityscape");
         
@@ -22,30 +24,53 @@ public class CityScapeViewer
         frame.add(component);
         
         // make the frame visible which will result in the paintComponent method being invoked on the
-        // component.
+        //  component.
         frame.setVisible(true);
         //for
+        int counter=0;
         
-        for (int i=0; i < 10; i++)
-        {
-            int count = 0;
-            System.out.println("count: " + count++);
-            
-            try
-            {
-                Thread.sleep(800);
-                xCar=xCar-100;
-                xCar2=xCar2+100;
-                component.repaint();
-                Thread.sleep(800);
-            } 
-            catch (Exception e)
-            {
-                e.printStackTrace();
+        	for (int i=0; i < 12; i++)
+            {	
+            	counter++;
+            	
+    	        try
+    	        {    	        	
+    				Thread.sleep(500);
+    				xCar-=100;
+    				xCar2+=100;
+    				
+    				if (counter<5)
+    				{
+    					birdX +=100;
+    					birdY +=50;
+    					
+    				}
+    				
+    				else if(counter > 5 && counter < 9 )
+    				{
+    					birdX +=100;
+    					birdY -=50;
+    					
+    				}
+    				
+    				else
+    				{
+    					birdX += 100;
+    					
+    				}
+    				
+    				component.repaint();
+    				Thread.sleep(500);
+    			} catch (Exception e) 
+    		        {
+    					e.printStackTrace();
+    				}
             }
-        }
-        
-        
-        }
+        }        
 
-}
+	        
+    }
+
+
+
+
